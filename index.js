2704ef2275bc44a80c8b2e1e2da88d11f758b1bb
DipongkarRoy770 <dipongkarroy233@gmail.com>
2023-02-12 20:53:53 +0600

    html work done

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ document.getElementById('btn').addEventListener('click',function(){
   const userEmailLogin = userEmail.value;
   const userPassword = document.getElementById('user-password');
   const passwordSet = userPassword.value;
-  console.log(userEmailLogin,passwordSet)
+  if(userEmailLogin==='dipongkarroy233@gmail.com' && passwordSet==='secret'){
+    window.location.href = 'http://127.0.0.1:5500/main.html'
+
+  }
+  else{
+    alert('password vule geso,baper taka tulte parbe na.')
+  }
 })
