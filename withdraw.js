72147bc90c36cace088dd1adffee3491607b1c51
DipongkarRoy770 <dipongkarroy233@gmail.com>
2023-02-13 17:43:11 +0600

    ok done

--- withdraw.js
+++ withdraw.js
@@ -1,21 +1,35 @@
+ //1step :btn niya kaj korci
  document.getElementById('withdraw').addEventListener('click',function(){
-  //input niya kaj korbo
+  //2step :input niya kaj korbo
   const input = document.getElementById('withdraw-input');
   const newInput = input.value ;
   const newInputuser = parseFloat(newInput)
-  //withdraw :
+  //7step:valu faka kora;
+  input.value = '' ;
+  //3step: withdraw : withdraw manu k id diye dorci 
   const withdrawUser = document.getElementById('withdraw-user')
   const newWithdraw = withdrawUser.innerText ;
   const newWithdraws  = parseFloat(newWithdraw)
+  //4step:taarpor previousBalance & newBalance sum korte hbe
   const withdrawBalance = newInputuser + newWithdraws ;
-  withdrawUser.innerText = withdrawBalance ;
-  //withdraw balance theke mainass korte hbe :
+  
+ 
+  // if(withdrawBalance>totalWithdraw){
+  //   alert('amr kase tk nai')
+  // }
+  //5step:withdraw balance theke mainass korte hbe :
   const balance = document.getElementById('balance');
   const newBalance = balance.innerText ;
   const userBalance = parseFloat(newBalance);
+  //6step:total theke withdraw amound bad dite hbe.
   const totalWithdraw = userBalance-withdrawBalance ;
+  
+  //akta condition diye kaj korte hbe
+  if(withdrawBalance>totalWithdraw){
+    
+    alert('amr kase  nai')
+  return 
+  }
+  withdrawUser.innerText = withdrawBalance ;
   balance.innerText = totalWithdraw ;
-  //valu faka kora;
-  input.value = '' ;
-
  })
